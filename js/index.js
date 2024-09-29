fe1596bda34e4793551789a67b7663438e31d619
Alexander Neo <AlexanderNeo427@gmail.com>
2024-09-29 21:55:40 +0800

    Testimonial section looks decent - gonna work on something else for now

--- js/index.js
+++ js/index.js
@@ -2,26 +2,52 @@ const NAV_LINKS = {
     HOME: "",
 }
 
-function makeTestimonial(testimonialData) {
+/*
+containerNode 
+    contentNode 
+    starContainer
+        filledStar, filledStar, emptyStar
+    name
+    title
+ */
+function makeTestimonial(testimonialDatum) {
     const containerNode = document.createElement("div")
     containerNode.classList.add("carousel-item")
 
     const contentNode = document.createElement("span")
-    contentNode.innerHTML = testimonialData.reviewer
+    contentNode.classList.add("content")
+    contentNode.innerHTML = "\"" + testimonialDatum.content + "\""
     containerNode.appendChild(contentNode)
 
     const NUM_STARS = 5
-    const filledStars = Math.min(testimonialData.rating, NUM_STARS)
-    const emptyStars = NUM_STARS - filledStars
+    const numFilledStars = Math.min(testimonialDatum.rating, NUM_STARS)
+    const numEmptyStars = NUM_STARS - numFilledStars
 
     const starContainer = document.createElement("div")
-    
+    starContainer.classList.add("star-container")
+    containerNode.appendChild(starContainer)
+    for (let i = 0; i < numFilledStars; i++) {
+        const filledStar = document.createElement("div") 
+        filledStar.innerHTML = "★"
+        starContainer.appendChild(filledStar)
+    }
+    for (let i = 0; i < numEmptyStars; i++) {
+        const emptyStar = document.createElement("div") 
+        emptyStar.innerHTML = "☆"
+        starContainer.appendChild(emptyStar)
+    }
 
-    const nameNode = document.createElement("h3")
-    nameNode.innerHTML = testimonialData.content
+    const nameNode = document.createElement("span")
+    nameNode.classList.add("name")
+    nameNode.innerHTML = testimonialDatum.reviewer
     containerNode.appendChild(nameNode)
 
-    return divNode
+    const titleNode = document.createElement("span")
+    titleNode.classList.add("title")
+    titleNode.innerHTML = testimonialDatum.title
+    containerNode.appendChild(titleNode)
+
+    return containerNode 
 }
 
 window.onload = function () {
