4264685029aca65a4b630089b9c67e6923de8ad1
Alexander Neo <AlexanderNeo427@gmail.com>
2024-10-03 22:13:33 +0800

    Refactoring all commonality into the globalVars css file. Created rental page, nothing done yet....

--- database/cars.js
+++ database/cars.js
@@ -1,10 +1,202 @@
 const allCarData = [
     {
         "id": 1,
-        "name": "Supracar"
+        "make": "Ford",
+        "model": "Falcon",
+        "year": 2016,
+        // "type": "Hatchback",
+        "seats": 5,
+        "imageURL": "",
+        "hourlyRate": 20
+    },
+    {
+        "id": 12,
+        "make": "Ford",
+        "model": "Strada",
+        "year": 2024,
+        // "type": "Hatchback",
+        "seats": 5,
+        "imageURL": "",
+        "hourlyRate": 20
     },
     {
         "id": 2,
-        "name": "Regulracar"
-    }
+        "make": "Toyota",
+        "model": "HiLux",
+        "year": 2024,
+        // "type": "Hatchback",
+        "seats": 5,
+        "imageURL": "",
+        "hourlyRate": 20
+    },
+    {
+        "id": 13,
+        "make": "Toyota",
+        "model": "Camry",
+        "year": 2024,
+        // "type": "Hatchback",
+        "seats": 5,
+        "imageURL": "",
+        "hourlyRate": 20
+    },
+    {
+        "id": 14,
+        "make": "Toyota",
+        "model": "Tacoma",
+        "year": 2024,
+        // "type": "Hatchback",
+        "seats": 5,
+        "imageURL": "",
+        "hourlyRate": 20
+    },
+    {
+        "id": 15,
+        "make": "Toyota",
+        "model": "Prius",
+        "year": 2024,
+        // "type": "Hatchback",
+        "seats": 5,
+        "imageURL": "",
+        "hourlyRate": 20
+    },
+    {
+        "id": 16,
+        "make": "Toyota",
+        "model": "Sienna",
+        "year": 2024,
+        // "type": "Hatchback",
+        "seats": 5,
+        "imageURL": "",
+        "hourlyRate": 20
+    },
+    {
+        "id": 3,
+        "make": "Toyota",
+        "model": "Corolla",
+        "year": 2018,
+        // "type": "Hatchback",
+        "seats": 5,
+        "imageURL": "",
+        "hourlyRate": 20
+    },
+    {
+        "id": 4,
+        "make": "Volkswagen",
+        "model": "Golf",
+        "year": 2024,
+        // "type": "Hatchback",
+        "seats": 5,
+        "imageURL": "",
+        "hourlyRate": 20
+    },
+    {
+        "id": 4,
+        "make": "Volkswagen",
+        "model": "Polo",
+        "year": 2024,
+        // "type": "Hatchback",
+        "seats": 5,
+        "imageURL": "",
+        "hourlyRate": 20
+    },
+    {
+        "id": 4,
+        "make": "Volkswagen",
+        "model": "Tiguan",
+        "year": 2024,
+        // "type": "Hatchback",
+        "seats": 5,
+        "imageURL": "",
+        "hourlyRate": 20
+    },
+    {
+        "id": 5,
+        "make": "Renault",
+        "model": "Clio",
+        "year": 2024,
+        // "type": "Hatchback",
+        "seats": 5,
+        "imageURL": "",
+        "hourlyRate": 20
+    },
+    {
+        "id": 5,
+        "make": "Renault",
+        "model": "Fuego",
+        "year": 2024,
+        // "type": "Hatchback",
+        "seats": 5,
+        "imageURL": "",
+        "hourlyRate": 20
+    },
+    {
+        "id": 5,
+        "make": "Renault",
+        "model": "Megane",
+        "year": 2024,
+        // "type": "Hatchback",
+        "seats": 5,
+        "imageURL": "",
+        "hourlyRate": 20
+    },
+    {
+        "id": 6,
+        "make": "Fiat",
+        "model": "Uno",
+        "year": 2024,
+        // "type": "Hatchback",
+        "seats": 5,
+        "imageURL": "",
+        "hourlyRate": 20
+    },
+    {
+        "id": 10,
+        "make": "Fiat",
+        "model": "Barchetta",
+        "year": 2024,
+        // "type": "Hatchback",
+        "seats": 5,
+        "imageURL": "",
+        "hourlyRate": 20
+    },
+    {
+        "id": 11,
+        "make": "Fiat",
+        "model": "Punto",
+        "year": 2024,
+        // "type": "Hatchback",
+        "seats": 5,
+        "imageURL": "",
+        "hourlyRate": 20
+    },
+    {
+        "id": 7,
+        "make": "Volvo",
+        "model": "850R",
+        "year": 2024,
+        // "type": "Hatchback",
+        "seats": 5,
+        "imageURL": "",
+        "hourlyRate": 20
+    },
+    {
+        "id": 8,
+        "make": "Volvo",
+        "model": "P1800",
+        "year": 2024,
+        // "type": "Hatchback",
+        "seats": 5,
+        "imageURL": "",
+        "hourlyRate": 20
+    },
+    {
+        "id": 9,
+        "make": "Volvo",
+        "model": "XC60",
+        "year": 2024,
+        // "type": "Hatchback",
+        "seats": 5,
+        "imageURL": "",
+        "hourlyRate": 20
+    },
 ]
---
+++ js/carCatalogue.js
@@ -0,0 +1,22 @@
+function appendChild(parent, child) {
+    parent.appendChild(child)
+    return child
+}
+
+/*
+div: car-card-container 
+    div: card-header
+        div: top-left
+            rating: div
+    img: car-img
+    div: car-info
+        h2: car-make-model
+
+ */
+function makeCard(carDatum) {
+
+}
+
+window.onload = function() {
+
+}
