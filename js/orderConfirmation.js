2e87efaec5dde987576d26c68769069efc0927cd
Alexander Neo <AlexanderNeo427@gmail.com>
2024-10-17 18:53:08 +0800

    Added functionality for random string generation of order ID - page re-directs after countdown

---
+++ js/orderConfirmation.js
@@ -0,0 +1,30 @@
+function generateRandomString(length) {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        result += characters.charAt(randomIndex);
+    }
+    return result;
+}
+
+function initializeTimer() {
+    let timeLeftSeconds = 15
+    const timerElem = document.querySelector(".order-confirmation-body .countdown-timer")
+    timerElem.textContent = timeLeftSeconds
+
+    const decrementTimer = function () {
+        timeLeftSeconds -= 1
+        timerElem.textContent = timeLeftSeconds
+        if (timeLeftSeconds <= 0) {
+            window.location.href = "index.html"
+        }
+        setTimeout(decrementTimer, 1000)
+    }
+    decrementTimer()
+}
+
+window.addEventListener('load', function () {
+    document.querySelector(".order-confirmation-body .ID-value").textContent = '#' + generateRandomString(10)
+    initializeTimer()
+})
