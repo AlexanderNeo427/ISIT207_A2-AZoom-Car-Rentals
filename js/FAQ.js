9e78a15e8d16b3e9fac9202c941eda864ab05462
Alexander Neo <AlexanderNeo427@gmail.com>
2024-10-18 04:00:54 +0800

    FAQ page now uses default "details" + "summary" elements for their dropdowns Looks like poo currently (no animation) but will (maybe)(possibly) fix that in the future (...maybe not)

--- js/FAQ.js
+++ js/FAQ.js
@@ -46,27 +46,27 @@ function makeDropdownObject(faqDatum) {
 }
 
 window.addEventListener('load', function () {
-    const MAX_HEIGHT = "45rem"
+    // const MAX_HEIGHT = "45rem"
 
-    console.log("START of the FAQ main function()")
-    const dropdownContainerNode = document.getElementsByClassName("all-dropdowns")[0]
-    database.frequentlyAskedQuestions.forEach(faqDatum => {
-        const newDropdownElem = appendChild(
-            dropdownContainerNode,
-            makeDropdownObject(faqDatum)
-        )
-        newDropdownElem.style.maxHeight = ""
-        const arrowNode = newDropdownElem.getElementsByClassName("updown-arrow")[0]
-        const dropdownState = new DropdownState(faqDatum.id)
+    // console.log("START of the FAQ main function()")
+    // const dropdownContainerNode = document.getElementsByClassName("all-dropdowns")[0]
+    // database.frequentlyAskedQuestions.forEach(faqDatum => {
+    //     const newDropdownElem = appendChild(
+    //         dropdownContainerNode,
+    //         makeDropdownObject(faqDatum)
+    //     )
+    //     newDropdownElem.style.maxHeight = ""
+    //     const arrowNode = newDropdownElem.getElementsByClassName("updown-arrow")[0]
+    //     const dropdownState = new DropdownState(faqDatum.id)
 
-        // Register click handler
-        newDropdownElem.onclick = function() {
-            dropdownState.isOpen = !dropdownState.isOpen     
-            newDropdownElem.style.maxHeight = dropdownState.isOpen ? MAX_HEIGHT : "" 
-            arrowNode.innerHTML = dropdownState.isOpen ? "🢁" : "🢃"
-            console.log(dropdownState.id, " ", dropdownState.isOpen)
-        }
-    })
+    //     // Register click handler
+    //     newDropdownElem.onclick = function() {
+    //         dropdownState.isOpen = !dropdownState.isOpen     
+    //         newDropdownElem.style.maxHeight = dropdownState.isOpen ? MAX_HEIGHT : "" 
+    //         arrowNode.innerHTML = dropdownState.isOpen ? "🢁" : "🢃"
+    //         console.log(dropdownState.id, " ", dropdownState.isOpen)
+    //     }
+    // })
 
-    console.log("END of the FAQ Main function()")
+    // console.log("END of the FAQ Main function()")
 })
